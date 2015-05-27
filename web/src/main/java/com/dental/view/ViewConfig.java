1450d2c19c2c9ff0dbf4318b6d749cbccbd2d81e
vrudyk <vrudyk@romexsoft.com>
2015-05-27 22:42:12 +0300

    viewconfig

---
+++ web/src/main/java/com/dental/view/ViewConfig.java
@@ -0,0 +1,12 @@
+package com.dental.view;
+
+/**
+ * Created by admin on 26.05.2015.
+ */
+public class ViewConfig {
+
+  public static final String FOLDER_PROFILE = "/profile";
+
+  public static final String FOLDER_ADMIN = "/admin";
+  public static final String FOLDER_AUTH = "/auth";
+}
