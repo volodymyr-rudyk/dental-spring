bb4b99961b8637167a95c6fcf5ae86a03e098da8
light <vlight88@gmail.com>
2015-07-05 21:35:23 +0300

    Expand exception handler

--- web/src/main/java/com/dental/controller/BaseController.java
+++ web/src/main/java/com/dental/controller/BaseController.java
@@ -4,8 +4,13 @@ import com.dental.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by light on 1/8/2015.
@@ -19,13 +24,18 @@ public abstract class BaseController {
     protected MessageSource messageSource;
 
     protected String renderView(String view) {
-      return new StringBuilder(getViewFolder()).append("/").append(view).toString();
+      return getViewFolder() + "/" + view;
     }
 
   protected abstract String getViewFolder();
 
     @ExceptionHandler(NotFoundException.class)
-    public String notFound() {
+    public String notFound(NotFoundException e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String url = e.getUrl();
+        if (!StringUtils.isEmpty(url)) {
+            // TODO add logger
+            response.sendRedirect(url);
+        }
         return "404";
     }
 
