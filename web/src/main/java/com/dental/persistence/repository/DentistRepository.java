fee02808210336fbdf964c9e8b5f5c3142f9aa5f
vrudyk <vrudyk@romexsoft.com>
2016-04-13 22:38:39 +0300

    refactoring add repos services

---
+++ web/src/main/java/com/dental/persistence/repository/DentistRepository.java
@@ -0,0 +1,9 @@
+package com.dental.persistence.repository;
+
+import com.dental.persistence.entity.Dentist;
+
+/**
+ * Created by admin on 27.05.2015.
+ */
+public interface DentistRepository extends GenericRepository<Dentist> {
+}
