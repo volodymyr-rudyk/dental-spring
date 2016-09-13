73634adee84803c34baf4dfcfed30fdaff6e40be
vrudyk <vrudyk@romexsoft.com>
2016-09-13 16:56:22 +0300

    dashboard rest remove accept json header

--- web/src/main/java/com/dental/web/rest/DashboardRestController.java
+++ web/src/main/java/com/dental/web/rest/DashboardRestController.java
@@ -30,8 +30,7 @@ public class DashboardRestController extends BaseRestController {
   @Autowired
   private PatientService patientService;
 
-  @RequestMapping(value = "/dashboard", method = { RequestMethod.POST, RequestMethod.GET },
-    produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+  @RequestMapping(value = "/dashboard", method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<?> dashboard(@LoggedDentist Dentist loggedDentist) {
     Dentist dentist = dentistService.get(loggedDentist.getId());
     Long patientsCount = patientService.patientsCount(loggedDentist.getId());
