1bfb32629b16116eff8e0da8b58273a241b68bf6
vrudyk <vrudyk@romexsoft.com>
2016-09-21 12:28:31 +0300

    add put rest for update dentist profile

--- web/src/main/java/com/dental/web/rest/ProfileRestController.java
+++ web/src/main/java/com/dental/web/rest/ProfileRestController.java
@@ -1,5 +1,6 @@
 package com.dental.web.rest;
 
+import com.dental.bean.DentistBean;
 import com.dental.init.LoggedDentist;
 import com.dental.persistence.entity.Dentist;
 import com.dental.service.DentistService;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,18 +24,28 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class ProfileRestController extends BaseRestController {
 
-  private Logger LOG = LoggerFactory.getLogger(PasswordRestController.class);
+  private static final Logger LOG = LoggerFactory.getLogger(PasswordRestController.class);
 
   @Autowired
   private DentistService dentistService;
 
   @RequestMapping(value = "/profile", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE,
       consumes = MediaType.ALL_VALUE)
-  public ResponseEntity<?> profile(HttpServletRequest httpServletRequest, @LoggedDentist Dentist loggedDentist) {
+  public ResponseEntity<?> profileGet(HttpServletRequest httpServletRequest, @LoggedDentist Dentist loggedDentist) {
     Dentist dentist = dentistService.get(loggedDentist.getId());
     ProfileDTO profileDTO = DTOUtils.convertToProfile(dentist);
 
     return success(profileDTO);
+  }
+
+  @RequestMapping(value = "/profile", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> profilePut(HttpServletRequest httpServletRequest, @LoggedDentist Dentist loggedDentist,
+                                      @RequestBody DentistBean dentistBean) {
+    Dentist dentist = dentistService.get(loggedDentist.getId());
+    dentistService.update(dentistBean, dentist);
+    LOG.info("Profile updated ...");
+    return success();
   }
 
 }
