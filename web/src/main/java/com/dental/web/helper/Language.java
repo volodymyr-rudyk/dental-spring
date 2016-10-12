b7fb4dac838206c79087899ad70646eb582db4dd
vrudyk <vrudyk@romexsoft.com>
2016-10-13 01:12:53 +0300

    Language

---
+++ web/src/main/java/com/dental/web/helper/Language.java
@@ -0,0 +1,28 @@
+package com.dental.web.helper;
+
+/**
+ * Created by vrudyk on 12.10.2016.
+ */
+public enum Language {
+    EN("en"),
+    UA("ua");
+
+    private String languageCode;
+
+    Language(String languageCode) {
+        this.languageCode = languageCode;
+    }
+
+    public String code() {
+        return languageCode;
+    }
+
+    public static Language from(String value) {
+        for (Language lng : values()){
+            if (lng.languageCode.equals(value)) {
+                return lng;
+            }
+        }
+        return EN;
+    }
+}
---
+++ web/src/main/java/com/dental/web/helper/ResponseStatus.java
@@ -0,0 +1,9 @@
+package com.dental.web.helper;
+
+/**
+ * Created by vrudyk on 11/20/2015.
+ */
+public enum ResponseStatus {
+  Success,
+  Failure
+}
